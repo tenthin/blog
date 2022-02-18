aa62363ee8bb63bd0785f575644fe770310c9a77
Tenzin <tenthinten@gmail.com>
2022-02-18 13:37:51 +0530

    add navbar scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar'
+import NavigationBar from './components/NavigationBar'
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <NavigationBar/>
     </div>
   );
 }
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavigationBar.scss';
+
+
+function NavigationBar() {
+  return (
+    <div>
+        <Navbar className="navbar" bg="dark" variant="dark">
+                <Navbar.Brand>
+                    Tenzin
+                </Navbar.Brand>
+        </Navbar>
+    </div>
+  )
+}
+
+
+export default NavigationBar;

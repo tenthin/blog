0bef510437b536dfe4f141e6113926071be162e2
Tenzin <tenthinten@gmail.com>
2022-02-18 13:28:50 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import Header from './Header';
-
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Navbar/>
     </div>
   );
 }
